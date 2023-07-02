fe0e18b6d46476cf4c7e3e8c54f9f053c1dbac79
darik666 <tattsar666@yandex.ru>
2023-07-02 16:09:29 +0300

    TZ16-ExceptionCorrections

--- gateway/src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -2,14 +2,12 @@ package ru.practicum.shareit;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.booking.UnsupportedStatusException;
 
 import javax.validation.ConstraintViolationException;
-import java.util.NoSuchElementException;
 
 /**
  * Обработчик исключений
@@ -20,23 +18,8 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse methodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        log.debug("Получен статус 400 Bad Request {}", e.getMessage(), e);
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-        return errorResponse;
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String validationConstraintException(final ConstraintViolationException e) {
         log.debug("Получен статус 400 Bad Request {}", e.getMessage(), e);
-        return e.getMessage();
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String validationNoSuchElementException(final NoSuchElementException e) {
-        log.debug("Получен статус 404 Not Found {}", e.getMessage(), e);
         return e.getMessage();
     }
 
